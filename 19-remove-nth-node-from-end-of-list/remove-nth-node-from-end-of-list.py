50648821ca075fb603a1c44c95095b7dad826eae
Swaroop  S <swaroop.s.iot.2023@snsce.ac.in>
2026-07-14 21:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (62.76%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        slow = dummy
+        fast = dummy
+
+        for i in range(n + 1):
+            fast = fast.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
