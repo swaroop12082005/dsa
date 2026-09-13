1965e9472b0c71d27a982879499df4db0702573a
Swaroop  S <swaroop.s.iot.2023@snsce.ac.in>
2026-09-13 15:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.52%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,9 +1,13 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-        i = 0
 
-        for ch in t:
-            if i < len(s) and s[i] == ch:
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+
+            if s[i] == t[j]:
                 i += 1
+
+            j += 1
 
         return i == len(s)
